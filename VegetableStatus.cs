618804a2548e03297053312797044411dc2c8ea2
Jessica Chammas <jessica.chammasm@gmail.com>
2024-11-05 18:02:49 -0800

    Add VegetableStatus.cs

---
+++ VegetableStatus.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class VegetableStatus : MonoBehaviour
+{
+    public bool isSliced = false;   // Tracks if the vegetable has been sliced
+    public bool hasSpawned = false; // Tracks if the vegetable was spawned
+    public bool isActive = false;   // Tracks if the vegetable is visible on screen
+}
